b987fa59102d9cc22c041994b2a5394eafa7b642
Abhinav-FP <a.mathur@futureprofilez.com>
2024-04-26 15:47:28 +0530

    player listing

--- controller/PlayerData.js
+++ controller/PlayerData.js
@@ -17,22 +17,21 @@ const options = [
 ];
 
 
-function searchFile(folderPath, fileName, callback) {
-  fs.readdir(folderPath, (err, files) => {
-      if (err) {
-          return callback(err);
-      }
-
-      const foundFile = files.find(file => file.startsWith(fileName));
-
-      if (!foundFile) {
-          return callback(null, null);
-      }
-
-      const filePath = path.join(folderPath, foundFile);
-      callback(null, filePath);
-  });
-}
+// function searchFile(folderPath, fileName, next) {
+//   fs.readdir(folderPath, (err, files) => {
+//       const foundFile = files.find(file => file.startsWith(fileName));
+//       if (foundFile) {
+//         console.log("file found")
+//         return true
+//         next()
+//       } else {
+//         console.log("file not found")
+//         return false
+//       }
+//       // const filePath = path.join(folderPath, foundFile);
+//       // callback(null, filePath);
+//   });
+// }
 
 // function searchFileInFolder(folderPath, fileName, callback) {
 //   fs.readdir(folderPath, (err, files) => {
@@ -62,9 +61,10 @@ function searchFile(folderPath, fileName, callback) {
 const upload = multer();
 
 exports.add = catchAsync(async (req, res) => {
-  console.log("req.body", req.body);
+  // console.log("req.body", req.body);
 
   // Check if FormData has any fields
+  try{
   if (!req.body || Object.keys(req.body).length === 0) {
     return res.status(400).json({
       message: "No Data Sent",
@@ -72,12 +72,19 @@ exports.add = catchAsync(async (req, res) => {
     });
   }
 
-  const { category, group, data, updated_at, pin } = req.body;
+  const { category, group, data, pin } = req.body;
   let searchTerm = `${category}${group}`;
   if (!data) {
     res.status(500).json({
       status: false,
       message: "PDF is not sent",
+    });
+  }
+  if(pin!=process.env.SECRET_PIN)
+  {
+    res.status(500).json({
+      status: false,
+      message: "Invalid Secret Pin",
     });
   }
 
@@ -93,7 +100,7 @@ exports.add = catchAsync(async (req, res) => {
 
   // File path
   const filePath = `./utils/PlayerData/${category}${group}.json`;
-  const jsonData = JSON.stringify(data);
+  const jsonData = JSON.parse(data);
 
   // Write data to the file
   fs.writeFile(filePath, jsonData, (err) => {
@@ -112,41 +119,57 @@ exports.add = catchAsync(async (req, res) => {
       message: "Data added successfully",
     });
   });
+}catch(error){
+  console.log("Facing Error:")
+  res.status(500).json({
+    status: false,
+    message: "Error in adding data",
+  });
+}
 });
 
 
-exports.list = catchAsync(async (req, res) => {
-  console.log("req.body", req.params.id);
 
-  // Check if FormData has any fields
-  if (!req.body || Object.keys(req.body).length === 0) {
-      return res.status(400).json({
-          message: "No Data Sent",
-          status: false,
-      });
+exports.list = catchAsync(async (req, res, next) => {
+
+  const fileName = req.params.id;
+  const folderPath = `./utils/PlayerData/${fileName}`; // Path to the folder
+
+  // console.log("folderPath", folderPath)
+  // const found =  searchFile(folderPath, fileName, next);
+  // const filePath = files.find(file => file === fileName);
+  // const fileStream = fs.createReadStream(`${folderPath}.json`);
+  try{
+    const fileStream = fs.readFileSync(`${folderPath}.json`, 'utf-8')
+    // console.log("fileStream",fileStream)
+    // const read = fileStream.pipe(res);
+    res.json({
+      status: true,
+      msg: "data retreived",
+      content: JSON.parse(fileStream)
+    })
+  }catch(error){
+    res.json({
+      status: false,
+      msg: "File not found",
+    })    
   }
+  // console.log("found",found)
+  //   if (found) {
 
-  const fileName=req.params.id;
-  const folderPath = './utils/PlayerData'; // Path to the folder
+  //       const fileStream = fs.createReadStream(filePath);
+  //       const read = fileStream.pipe(res);
+  //       console.log("read")
 
-  searchFile(folderPath, fileName, (err, filePath) => {
-      if (err) {
-          console.error('Error:', err.message);
-          return res.status(500).json({
-              message: 'Error searching for file',
-              status: false,
-          });
-      }
 
-      if (!filePath) {
-          return res.status(404).json({
-              message: 'File not found',
-              status: false,
-          });
-      }
-
-      // Send the file as a response
-      const fileStream = fs.createReadStream(filePath);
-      fileStream.pipe(res);
-  });
+  //       // return res.status(500).json({
+  //       //     message: 'Error searching for file',
+  //       //     status: false,
+  //       // });
+  //   }else {
+  //       return res.status(404).json({
+  //           message: 'File not found',
+  //           status: false,
+  //       });
+    // }
 });
