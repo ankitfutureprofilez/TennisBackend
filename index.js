0f2bf60ff6daf8fea65b898bf1d4cdfb0175a61d
Abhinav-FP <a.mathur@futureprofilez.com>
2024-04-29 18:16:54 +0530

    tennis khelo url added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 
 const corsOptions = {
-  origin: 'https://tenniskhelo.com',
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, 
 };
