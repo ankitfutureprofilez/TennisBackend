4c171e1a4231f0e81d82236fb49afbd271f3c8eb
Abhinav-FP <a.mathur@futureprofilez.com>
2024-04-29 15:15:14 +0530

    player listing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(fileUpload());
 const cors = require("cors");
 const corsOptions = {
-  origin: '*',
+  origin: 'https://tenniskhelo.com/',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, 
 };
