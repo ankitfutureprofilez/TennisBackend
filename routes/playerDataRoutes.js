ae8c2257520f8587d64dc75136ed2717f3313583
Abhinav-FP <a.mathur@futureprofilez.com>
2024-04-30 11:25:00 +0530

    allow cors added

--- routes/playerDataRoutes.js
+++ routes/playerDataRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const {add, list} = require("../controller/PlayerData.js")
 const fs = require('fs');
+import allowCors from "../middleware/cors.js";
 
-router.post("/add", add);
-router.get("/list/:id", list);
+router.post("/add", allowCors, add);
+router.get("/list/:id", allowCors, list);
 
 module.exports =router;
