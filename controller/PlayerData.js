d81949db99fa7e5fd91c18b202460fa28d5fd434
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-05-07 18:27:28 +0530

    aa

--- controller/PlayerData.js
+++ controller/PlayerData.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const catchAsync = require("../utils/catchAsync");
 const PlayerRanking = require("../db/RankingData");
 const axios = require("axios");
@@ -13,9 +12,10 @@ const options = [
   "BU-12",
   "BU-14",
   "BU-16",
+  "BU-1",
   "BU-18",
   "Msingles",
-  "Mdoubles","Wsingles","Wdoubles"
+  "Mdoubles", "Wsingles", "Wdoubles"
 ];
 
 const upload = multer();
@@ -28,17 +28,14 @@ exports.add = catchAsync(async (req, res) => {
         status: false,
       });
     }
-
-    const { category, group, data, pin, text } = req.body;
+    const { category, group, data, pin, text,date } = req.body;
     let searchTerm = `${category}${group}`;
-     
     if (pin != process.env.SECRET_PIN) {
       res.status(400).json({
         status: false,
         message: "Invalid Secret Pin",
       });
     }
-
     const found = options.find((item) => item === searchTerm);
     if (!found) {
       res.status(400).json({
@@ -71,6 +68,7 @@ exports.add = catchAsync(async (req, res) => {
         category: category,
         group: group,
         json: text,
+        date:date ,
         updated_at : Date.now()
       });
       const result = await record.save();
@@ -88,27 +86,27 @@ exports.add = catchAsync(async (req, res) => {
       }
     }
 
-    // // File path
-    // const filePath = `${category}${group}.json`;
-    // const jsonData = JSON.parse(data);
+    // File path
+    const filePath = `${category}${group}.json`;
+    const jsonData = JSON.parse(data);
 
-    // // Write data to the file
-    // fs.writeFile(filePath, jsonData, (err) => {
-    //   if (err) {
-    //     // Handle error
-    //     console.error("Error writing file:", err);
-    //     res.status(500).json({
-    //       status: false,
-    //       message: "Error while adding data !!.",
-    //     });
-    //     return;
-    //   }
-    //   console.log("File created or updated successfully");
-    //   res.status(200).json({
-    //     status: true,
-    //     message: "Data added successfully",
-    //   });
-    // });
+    // Write data to the file
+    fs.writeFile(filePath, jsonData, (err) => {
+      if (err) {
+        // Handle error
+        console.error("Error writing file:", err);
+        res.status(500).json({
+          status: false,
+          message: "Error while adding data !!.",
+        });
+        return;
+      }
+      console.log("File created or updated successfully");
+      res.status(200).json({
+        status: true,
+        message: "Data added successfully",
+      });
+    });
   } catch (error) {
     console.log("Facing Error:");
     res.status(500).json({
@@ -118,17 +116,87 @@ exports.add = catchAsync(async (req, res) => {
   }
 });
 
+// exports.add = async (req, res) => {
+//   try {
+//     if (!req.body || Object.keys(req.body).length === 0) {
+//       return res.status(400).json({
+//         message: "No Data Sent",
+//         status: false,
+//       });
+//     }
+//     console.log("req.body,req.body",req.body)
+
+//     const { category, group, pin, text, date } = req.body;
+//     if (pin !== process.env.SECRET_PIN) {
+//       return res.status(400).json({
+//         status: false,
+//         message: "Invalid Secret Pin",
+//       });
+//     }
+
+//     let searchTerm = `${category}${group}`;
+//     console.log("Received data:", text); 
+//     const jsonData = JSON.parse(text);
+//     const playerRecords = [];
+//     let currentPlayer; 
+//     for (const player of jsonData) {
+//       currentPlayer = player; 
+//       const playerInfo = currentPlayer;
+//       console.log("Player Information:", playerInfo);
+//       const playerRecord = new PlayerRanking({
+//         name: searchTerm,
+//         category: category,
+//         group: group,
+//         json: playerInfo, 
+//         date: date,
+//         updated_at: Date.now()
+//       });
+//       try {
+//         console.log("playerRecord", playerRecord)
+//         const result = await playerRecord.save();
+//         console.log("result", result)
+//         console.log("Player Data Added Successfully");
+//         const daata = playerRecords.push(result);
+//         console.log("daata", daata)
+//       } catch (error) {
+//         console.error("Failed to add player data:", error);
+//         return res.status(500).json({
+//           status: false,
+//           message: "Failed to add player data",
+//           error: error
+//         });
+//       }
+//     }
+//     res.json({
+//       status: true,
+//       message: "Data Added Successfully",
+//       playerRecords: playerRecords
+//     });
+//   } catch (error) {
+//     console.log("Facing Error:", error);
+//     res.status(500).json({
+//       status: false,
+//       message: "Error in adding data",
+//     });
+//   }
+// };
+
+
+
 
 
 exports.list = catchAsync(async (req, res, next) => {
   const fileName = req.params.id;
+  console.log("fileName", fileName)
   try {
     let isPresent = await PlayerRanking.findOne({ name: fileName });
+    console.log("isPresent", isPresent)
     if (isPresent) {
       res.json({
         status: true,
         msg: "data retreived",
-        content: JSON.parse(isPresent?.json),
+        // content: isPresent?.json,
+         content: JSON?.parse(isPresent?.json),
         created_At: isPresent?.updated_at,
       });
     } else {
--- db/RankingData.js
+++ db/RankingData.js
@@ -1,11 +1,34 @@
+// const mongoose = require("mongoose");
+
+// const PlayerRankingSchema = mongoose.Schema({
+//   name: String,
+//   category: String,
+//   group: String,
+//   json: mongoose.Schema.Types.Mixed,
+//   updated_at: Date,
+//   date:String,
+// });
+
+// module.exports = mongoose.model("PlayerRanking", PlayerRankingSchema);
+
+
 const mongoose = require("mongoose");
 
 const PlayerRankingSchema = mongoose.Schema({
   name: String,
   category: String,
   group: String,
-  json: mongoose.Schema.Types.Mixed,
-  updated_at: Date
+  json: {
+    Name: String,
+    Registration: String,
+    DateOfBirth: String,
+    FinalScore: Number,
+    Rank: Number,
+    State: String
+  },
+  updated_at: Date,
+  date: String,
 });
 
 module.exports = mongoose.model("PlayerRanking", PlayerRankingSchema);
+
