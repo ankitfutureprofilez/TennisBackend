2d97f9a067efac003b89c741f30f40e5145f5fba
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-05-08 12:22:14 +0530

    aa

--- controller/PlayerData.js
+++ controller/PlayerData.js
@@ -21,151 +21,152 @@ const options = [
 
 const upload = multer();
 
-exports.add = catchAsync(async (req, res) => {
-  try {
-    if (!req.body || Object.keys(req.body).length === 0) {
-      return res.status(400).json({
-        message: "No Data Sent",
-        status: false,
-      });
-    }
-    const { category, group, data, pin, text,date } = req.body;
-    let searchTerm = `${category}${group}`;
-    if (pin != process.env.SECRET_PIN) {
-      res.status(400).json({
-        status: false,
-        message: "Invalid Secret Pin",
-      });
-    }
-    const found = options.find((item) => item === searchTerm);
-    if (!found) {
-      res.status(400).json({
-        message: "Invalid category or group selected !!",
-      });
-      return;
-    }
-    let isPresent = await PlayerRanking.findOne({ name: searchTerm });
-    if (isPresent) {
-      const updatedResult = await PlayerRanking.findOne({name: searchTerm });
-      updatedResult.updated_at =  Date.now();
-      updatedResult.json = text; 
-      updatedResult.group = group;
-      const updated = await updatedResult.save();
-      if (updated) {
-        res.json({
-          status: true,
-          updated:updated,
-          message: "Data Updated Successfully",
-        });
-      } else {
-        res.json({
-          status: false,
-          message: "Failed to update data",
-        });
-      }
-    } else {
-      const record = new PlayerRanking({
-        name: searchTerm,
-        category: category,
-        group: group,
-        json: text,
-        date:date ,
-        updated_at : Date.now()
-      });
-      const result = await record.save();
-      if (result) {
-        res.json({
-          status: true,
-          message: "Data Added Successfully",
-        });
-      } else {
-        res.json({
-          status: false,
-          error: result,
-          message: "Failed to add data",
-        });
-      }
-    }
-
-    // File path
-    const filePath = `${category}${group}.json`;
-    const jsonData = JSON.parse(data);
-
-    // Write data to the file
-    fs.writeFile(filePath, jsonData, (err) => {
-      if (err) {
-        // Handle error
-        console.error("Error writing file:", err);
-        res.status(500).json({
-          status: false,
-          message: "Error while adding data !!.",
-        });
-        return;
-      }
-      console.log("File created or updated successfully");
-      res.status(200).json({
-        status: true,
-        message: "Data added successfully",
-      });
-    });
-  } catch (error) {
-    console.log("Facing Error:");
-    res.status(500).json({
-      status: false,
-      message: "Error in adding data",
-    });
-  }
-});
-if (pin !== process.env.SECRET_PIN) {
-  return res.status(400).json({
-    status: false,
-    message: "Invalid Secret Pin",
-  });
-}
-// exports.add = async (req, res) => {
+// exports.add = catchAsync(async (req, res) => {
 //   try {
-//     const { category, group, pin, json_data, date } = req.body;
-
-
-//      let savedData ;
-//       console.log("json_data",JSON.parse(json_data))
-//       const json = JSON.parse(json_data)
-//       json.forEach(element => {
-
-//         console.log("element",element)
-//         const arr = [];
-//         const item = new RankingData({
-//           category: category,
-//           group: group,
-//           updated_at: date,
-//           name : element.Name || "dsfsd",
-//           dob : element.DOB,
-//         });
-//         arr.push(item)
-//         item.save();
-//         savedData = arr;
+//     if (!req.body || Object.keys(req.body).length === 0) {
+//       return res.status(400).json({
+//         message: "No Data Sent",
+//         status: false,
 //       });
-//       if (savedData) {
+//     }
+//     const { category, group, data, pin, text,date } = req.body;
+//     let searchTerm = `${category}${group}`;
+//     if (pin != process.env.SECRET_PIN) {
+//       res.status(400).json({
+//         status: false,
+//         message: "Invalid Secret Pin",
+//       });
+//     }
+//     const found = options.find((item) => item === searchTerm);
+//     if (!found) {
+//       res.status(400).json({
+//         message: "Invalid category or group selected !!",
+//       });
+//       return;
+//     }
+//     let isPresent = await PlayerRanking.findOne({ name: searchTerm });
+//     if (isPresent) {
+//       const updatedResult = await PlayerRanking.findOne({name: searchTerm });
+//       updatedResult.updated_at =  Date.now();
+//       updatedResult.json = text; 
+//       updatedResult.group = group;
+//       const updated = await updatedResult.save();
+//       if (updated) {
+//         res.json({
+//           status: true,
+//           updated:updated,
+//           message: "Data Updated Successfully",
+//         });
+//       } else {
+//         res.json({
+//           status: false,
+//           message: "Failed to update data",
+//         });
+//       }
+//     } else {
+//       const record = new PlayerRanking({
+//         name: searchTerm,
+//         category: category,
+//         group: group,
+//         json: text,
+//         date:date ,
+//         updated_at : Date.now()
+//       });
+//       const result = await record.save();
+//       if (result) {
 //         res.json({
 //           status: true,
 //           message: "Data Added Successfully",
 //         });
-//     } else {
-//       res.json({
-//         status: false,
-//         error: result,
-//         message: "Failed to add data",
-//       });
+//       } else {
+//         res.json({
+//           status: false,
+//           error: result,
+//           message: "Failed to add data",
+//         });
+//       }
 //     }
-     
+
+//     // File path
+//     const filePath = `${category}${group}.json`;
+//     const jsonData = JSON.parse(data);
+
+//     // Write data to the file
+//     fs.writeFile(filePath, jsonData, (err) => {
+//       if (err) {
+//         // Handle error
+//         console.error("Error writing file:", err);
+//         res.status(500).json({
+//           status: false,
+//           message: "Error while adding data !!.",
+//         });
+//         return;
+//       }
+//       console.log("File created or updated successfully");
+//       res.status(200).json({
+//         status: true,
+//         message: "Data added successfully",
+//       });
+//     });
 //   } catch (error) {
-//     console.log("Facing Error:", error);
+//     console.log("Facing Error:");
 //     res.status(500).json({
 //       status: false,
 //       message: "Error in adding data",
 //     });
 //   }
-// };
+// });
+exports.add = async (req, res) => {
+  try {
+    const { category, group, pin, json_data, date } = req.body;
+    if (pin !== process.env.SECRET_PIN) {
+      return res.status(400).json({
+        status: false,
+        message: "Invalid Secret Pin",
+      });
+    }
+     let savedData ;
+      console.log("json_data",JSON.parse(json_data))
+      const json = JSON.parse(json_data)
+      json.forEach(element => {
+        console.log("element",element)
+        const arr = [];
+        const item = new RankingData({
+          category: category,
+          group: group,
+          updatedAt: date,
+          name : element.Name ,
+          dob : element.DOB,
+          state:element?.State,
+          final:element?.Final,
+          rank:element?.Rank,
+          reg:element?.Reg,
+        });
+        arr.push(item)
+        item.save();
+        savedData = arr;
+      });
+      if (savedData) {
+        res.json({
+          status: true,
+          message: "Data Added Successfully",
+        });
+    } else {
+      res.json({
+        status: false,
+        error: result,
+        message: "Failed to add data",
+      });
+    }
+     
+  } catch (error) {
+    console.log("Facing Error:", error);
+    res.status(500).json({
+      status: false,
+      message: "Error in adding data",
+    });
+  }
+};
 
 exports.list = catchAsync(async (req, res, next) => {
   const fileName = req.params.id;
@@ -193,6 +194,34 @@ exports.list = catchAsync(async (req, res, next) => {
     //   msg: "data retreived",
     //   content: JSON.parse(fileStream)
     // })
+  } catch (error) {
+    res.json({
+      status: false,
+      msg: "File not found",
+    });
+  }
+});
+
+
+exports.playerlist = catchAsync(async (req, res,next) => {
+  const category = req.params.category;
+  const group = req.params.group;
+  console.log("fileName", group, category);
+  try {
+    let record = await PlayerRanking.find({ category, group }); 
+    console.log("record", record);
+    if (record) {
+      res.json({
+        status: true,
+        msg: "Data retrieved",
+        content: record,
+      });
+    } else {
+      res.json({
+        status: false,
+        msg: "No data available",
+      });
+    }
   } catch (error) {
     res.json({
       status: false,
--- routes/playerDataRoutes.js
+++ routes/playerDataRoutes.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
-const {add, list} = require("../controller/PlayerData.js")
+const {add, list,playerlist} = require("../controller/PlayerData.js")
 const fs = require('fs');
 
 router.post("/add", add);
+
 router.get("/list/:id", list);
+
+router.get("/playerlist/:category/:group", playerlist);
 
 module.exports =router;
